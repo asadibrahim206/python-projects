a5e6b75519f49cee82819b5139deba275e03a0aa
asadibrahim206 <asad.denin@gmail.com>
2025-12-29 06:23:58 +0500

    working with coordinate on opencage

--- practice.py
+++ practice.py
@@ -1,7 +1,33 @@
-name = "muhsin"
+#BEGINERS 
+#Given two integer numbers, write a Python program to return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# num1 = 3
+# num2 = 2
+
+# if num1 * num2 < 1000:
+#     print("the mulitiplication is : " ,num1 * num2)
+# else: 
+#     print("the sum is : ", num1 + num2)
+
+#Exercise 2: Print the Sum of a Current Number and a Previous number
 
 
-if name == "asad" or  "humza":
-    print("hello") 
-else:
-    print("bye")
+# prev_num = 0
+# for num in range(10):
+#     print(f"The current number is : {num} and the previous number is {prev_num} and the sum is : {num + prev_num}")
+#     prev_num = num
+
+# Exercise 3: Print characters present at an even index number
+
+# string = input("ENTER AN STRING : ")
+
+# for chr in range(len(string)):
+#     if chr % 2 == 0:
+#         print(string[chr])
+#     else
+#         print("noe even")
+
+
+name = input("ENTER YOUR NAME : ")
+for i in range(len(name)):   if i % 2 == 0:
+        print(name[i])
